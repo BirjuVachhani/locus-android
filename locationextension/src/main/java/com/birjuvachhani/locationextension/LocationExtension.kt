b9f6f4b3adccd88b8d8d32118d61004ae938e776
BirjuVachhani <brvachhani@gmail.com>
2018-11-12 20:26:39 +0530

    refactor: wrap extension code into geolocation class
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

--- locationextension/src/main/java/com/birjuvachhani/locationextension/LocationExtension.kt
+++ locationextension/src/main/java/com/birjuvachhani/locationextension/LocationExtension.kt
@@ -3,41 +3,86 @@ package com.birjuvachhani.locationextension
 import android.location.Location
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.location.LocationRequest
 
 /**
  * Created by Birju Vachhani on 09-11-2018.
  */
 
+class GeoLocation(val activity: AppCompatActivity, func: LocationOptions.() -> Unit = {}) {
+
+    private var options = LocationOptions()
+
+    init {
+        options(func)
+    }
+
+    fun options(func: LocationOptions.() -> Unit) {
+        val locationOptions = LocationOptions()
+        func.invoke(locationOptions)
+        locationOptions.request.invoke(locationOptions.locationRequest)
+        this.options = locationOptions
+    }
+
+    fun getCurrentLocation(success: (Location) -> Unit = {}) {
+        getLocationHelper(activity).apply {
+            Handler().post {
+                startLocationProcess(LocationOptions(), success, { _, _ -> }, true)
+            }
+        }
+    }
+
+    // Give location only once
+    fun getCurrentLocation(
+        success: (Location) -> Unit = {},
+        failure: (isDenied: Boolean, t: Throwable?) -> Unit = { _, _ -> }
+    ) {
+        getLocationHelper(activity).apply {
+            Handler().post {
+                startLocationProcess(options, success, failure, true)
+            }
+        }
+    }
+
+    fun listenForLocation(
+        success: (Location) -> Unit, failure: (isDenied: Boolean, t: Throwable?) -> Unit
+    ) {
+        getLocationHelper(activity).apply {
+            Handler().post {
+                startLocationProcess(options, success, failure, false)
+            }
+        }
+    }
+
+    private fun getLocationHelper(activity: AppCompatActivity): LocationHelper {
+        val frag = activity.supportFragmentManager.findFragmentByTag(LocationHelper.TAG)
+        return if (frag == null) {
+            val mLocationHelper = LocationHelper.newInstance()
+            activity.supportFragmentManager.beginTransaction()
+                .add(mLocationHelper, LocationHelper.TAG)
+                .commit()
+            mLocationHelper
+        } else {
+            frag as LocationHelper
+        }
+    }
+
+}
+
 // Give location only once
-fun AppCompatActivity.getCurrentLocation(
-    success: (Location) -> Unit, failure: (isDenied: Boolean, t: Throwable?) -> Unit
-) {
-    getLocationHelper(this).apply {
-        Handler().post {
-            startLocationProcess(success, failure, true)
-        }
-    }
-}
 
-fun AppCompatActivity.listenForLocation(
-    success: (Location) -> Unit, failure: (isDenied: Boolean, t: Throwable?) -> Unit
-) {
-    getLocationHelper(this).apply {
-        Handler().post {
-            startLocationProcess(success, failure, false)
-        }
-    }
-}
+class LocationOptions {
+    var rationale: String = "This a rationale string!"
+    var blocked: String = "Permission is blocked!"
+    var request: LocationRequest.() -> Unit = {}
+    internal var locationRequest = LocationRequest()
 
-private fun getLocationHelper(activity: AppCompatActivity): LocationHelper {
-    val frag = activity.supportFragmentManager.findFragmentByTag(LocationHelper.TAG)
-    if (frag == null) {
-        val mLocationHelper = LocationHelper.newInstance()
-        activity.supportFragmentManager.beginTransaction()
-            .add(mLocationHelper, LocationHelper.TAG)
-            .commit()
-        return mLocationHelper
-    } else {
-        return frag as LocationHelper
+    companion object {
+        fun build(func: LocationOptions.() -> Unit): LocationOptions {
+            val locationOptions = LocationOptions()
+            func.invoke(locationOptions)
+            locationOptions.request.invoke(locationOptions.locationRequest)
+            return locationOptions
+        }
     }
 }
