9634d9ebf883dae37ade429b69eb4639168271f4
BirjuVachhani <brvachhani@gmail.com>
2018-11-12 20:26:39 +0530

    refactor: wrap extension code into geolocation class
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

--- app/src/main/java/com/birjuvachhani/locationextensionsample/MainActivity.kt
+++ app/src/main/java/com/birjuvachhani/locationextensionsample/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
-import com.birjuvachhani.locationextension.getCurrentLocation
+import com.birjuvachhani.locationextension.GeoLocation
+import com.google.android.gms.location.LocationRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var count = 0
+    private val geoLocation = GeoLocation(this) {
+        rationale = "this is custom rationale"
+        blocked = "this is custom permission blocked message"
+        request = {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 1000
+            fastestInterval = 1000
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,17 +27,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun startTracking(v: View) {
-        getCurrentLocation(
-            { location ->
-                count++
-                tvLatitude.text = location.latitude.toString()
-                tvLongitude.text = location.longitude.toString()
-                tvCount.text = count.toString()
-                Log.e("MAINACTIVITY", "Latitude: ${location.latitude}\tLongitude: ${location.longitude}")
-            },
-            { isDenied, t ->
-                tvError.text = "Error: isDenied: " + isDenied + " throwable: " + t?.message
-                Log.e("MAINACTIVITY", "Error: isDenied: ${isDenied} throwable: ${t?.message}")
-            })
+
+        geoLocation.getCurrentLocation({ location ->
+            count++
+            tvLatitude.text = location.latitude.toString()
+            tvLongitude.text = location.longitude.toString()
+            tvCount.text = count.toString()
+            Log.e("MAINACTIVITY", "Latitude: ${location.latitude}\tLongitude: ${location.longitude}")
+        }, { isDenied, t ->
+
+        })
     }
 }
