8762b6253bbee53f7e8889088b1aef5dd5d75c77
BirjuVachhani <brvachhani@gmail.com>
2018-11-13 20:33:04 +0530

    feat: add location implementation
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

--- locationextension/src/main/java/com/birjuvachhani/locationextension/LocationExtension.kt
+++ locationextension/src/main/java/com/birjuvachhani/locationextension/LocationExtension.kt
@@ -1,9 +1,9 @@
 package com.birjuvachhani.locationextension
 
 import android.location.Location
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.location.LocationRequest
+
 
 /**
  * Created by Birju Vachhani on 09-11-2018.
@@ -12,8 +12,17 @@ import com.google.android.gms.location.LocationRequest
 class GeoLocation(val activity: AppCompatActivity, func: LocationOptions.() -> Unit = {}) {
 
     private var options = LocationOptions()
+    val INTERVAL_IN_MS = 1000L
+    private val FASTEST_INTERVAL_IN_MS = 5000L
+    private val MAX_WAIT_TIME_IN_MS = 5000L
+    private val NUMBER_OF_UPDATES = 1
 
     init {
+        options.locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        options.locationRequest.interval = INTERVAL_IN_MS
+        options.locationRequest.fastestInterval = FASTEST_INTERVAL_IN_MS
+        options.locationRequest.numUpdates = NUMBER_OF_UPDATES
+        options.locationRequest.maxWaitTime = MAX_WAIT_TIME_IN_MS
         options(func)
     }
 
@@ -24,33 +33,26 @@ class GeoLocation(val activity: AppCompatActivity, func: LocationOptions.() -> U
         this.options = locationOptions
     }
 
-    fun getCurrentLocation(success: (Location) -> Unit = {}) {
+    fun getCurrentLocation(success: (Location) -> Unit) {
         getLocationHelper(activity).apply {
-            Handler().post {
-                startLocationProcess(LocationOptions(), success, { _, _ -> }, true)
-            }
+            startLocationProcess(options, success, {}, true)
         }
     }
 
-    // Give location only once
     fun getCurrentLocation(
-        success: (Location) -> Unit = {},
-        failure: (isDenied: Boolean, t: Throwable?) -> Unit = { _, _ -> }
+        success: (Location) -> Unit,
+        failure: (LocationError) -> Unit
     ) {
         getLocationHelper(activity).apply {
-            Handler().post {
-                startLocationProcess(options, success, failure, true)
-            }
+            startLocationProcess(options, success, failure, true)
         }
     }
 
     fun listenForLocation(
-        success: (Location) -> Unit, failure: (isDenied: Boolean, t: Throwable?) -> Unit
+        success: (Location) -> Unit, failure: (error: LocationError) -> Unit
     ) {
         getLocationHelper(activity).apply {
-            Handler().post {
-                startLocationProcess(options, success, failure, false)
-            }
+            startLocationProcess(options, success, failure, false)
         }
     }
 
@@ -60,29 +62,23 @@ class GeoLocation(val activity: AppCompatActivity, func: LocationOptions.() -> U
             val mLocationHelper = LocationHelper.newInstance()
             activity.supportFragmentManager.beginTransaction()
                 .add(mLocationHelper, LocationHelper.TAG)
-                .commit()
+                .commitNow()
             mLocationHelper
         } else {
             frag as LocationHelper
         }
     }
 
+    fun stopTrackingLocation() {
+        getLocationHelper(activity).stopContinuousLocation()
+    }
 }
 
 // Give location only once
 
-class LocationOptions {
-    var rationale: String = "This a rationale string!"
-    var blocked: String = "Permission is blocked!"
-    var request: LocationRequest.() -> Unit = {}
-    internal var locationRequest = LocationRequest()
-
-    companion object {
-        fun build(func: LocationOptions.() -> Unit): LocationOptions {
-            val locationOptions = LocationOptions()
-            func.invoke(locationOptions)
-            locationOptions.request.invoke(locationOptions.locationRequest)
-            return locationOptions
-        }
-    }
-}
+data class LocationOptions(
+    var rationale: String = "Location permission is required in order to use this feature properly.Please grant the permission.",
+    var blocked: String = "Location permission is blocked. Please allow permission from settings screen to use this feature",
+    var request: LocationRequest.() -> Unit = {},
+    internal var locationRequest: LocationRequest = LocationRequest()
+)
