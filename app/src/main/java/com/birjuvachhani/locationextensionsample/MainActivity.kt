12da97b3857745707414383d48adb9a29aca6050
BirjuVachhani <brvachhani@gmail.com>
2018-11-13 20:33:04 +0530

    feat: add location implementation
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

--- app/src/main/java/com/birjuvachhani/locationextensionsample/MainActivity.kt
+++ app/src/main/java/com/birjuvachhani/locationextensionsample/MainActivity.kt
@@ -6,14 +6,13 @@ import android.util.Log
 import android.view.View
 import com.birjuvachhani.locationextension.GeoLocation
 import com.google.android.gms.location.LocationRequest
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = this::class.java.simpleName
+
     var count = 0
     private val geoLocation = GeoLocation(this) {
-        rationale = "this is custom rationale"
-        blocked = "this is custom permission blocked message"
         request = {
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
             interval = 1000
@@ -27,15 +26,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun startTracking(v: View) {
+//        geoLocation.getCurrentLocation({ location ->
+//            Log.e(TAG, "Latitude: ${location.latitude}\tLongitude: ${location.longitude}")
+//        }, {
+//            Log.e(TAG, "isDenied: ${it.isPermissionDenied}\t Error: ${it.throwable?.message}")
+//        })
 
-        geoLocation.getCurrentLocation({ location ->
-            count++
-            tvLatitude.text = location.latitude.toString()
-            tvLongitude.text = location.longitude.toString()
-            tvCount.text = count.toString()
-            Log.e("MAINACTIVITY", "Latitude: ${location.latitude}\tLongitude: ${location.longitude}")
-        }, { isDenied, t ->
-
+        geoLocation.listenForLocation({ location ->
+            Log.e(TAG, "Latitude: ${location.latitude}\tLongitude: ${location.longitude}")
+        }, {
+            Log.e(TAG, "isDenied: ${it.isPermissionDenied}\t Error: ${it.throwable?.message}")
         })
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        geoLocation.stopTrackingLocation()
     }
 }
