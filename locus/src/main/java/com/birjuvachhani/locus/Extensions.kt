ab18b096bd9636c19d5cd5620467ee3bbf7a2801
BirjuVachhani <brvachhani@gmail.com>
2019-07-24 15:14:48 +0530

    feat: add extension properties to check type of error
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

--- locus/src/main/java/com/birjuvachhani/locus/Extensions.kt
+++ locus/src/main/java/com/birjuvachhani/locus/Extensions.kt
@@ -32,3 +32,18 @@ fun <T : Any> LiveData<T?>.watch(owner: LifecycleOwner, func: (T) -> Unit) {
         result?.apply(func)
     })
 }
+
+// Extension property to check if the error caused because the user denied permission or not
+val Throwable.isDenied get() = this.message == Constants.DENIED
+
+// Extension property to check if the error caused because the user permanently denied permission or not
+val Throwable.isPermanentlyDenied get() = this.message == Constants.PERMANENTLY_DENIED
+
+// Extension property to check if the error caused because the location settings resolution failed or not
+val Throwable.isSettingsResolutionFailed get() = this.message == Constants.RESOLUTION_FAILED
+
+// Extension property to check if the error caused because the user denied to enable location or not
+val Throwable.isSettingsDenied get() = this.message == Constants.LOCATION_SETTINGS_DENIED
+
+// Extension property to check if the error caused because of some Fatal Exception or not
+val Throwable.isFatal get() = !isDenied && !isPermanentlyDenied && !isSettingsDenied && !isSettingsResolutionFailed
