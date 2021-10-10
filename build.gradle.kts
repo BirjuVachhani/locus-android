1d610e7391977fc41425481e081eeb7360d92270
Birju Vachhani <brvachhani@gmail.com>
2021-10-10 21:44:29 +0530

    Migrate to gradle kotlin scripts

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1") // For jit-pack
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
