29c4430df65f97ad01b0840adaa4e2698f42bfaf
birjuvachhani <brvachhani@gmail.com>
2022-01-11 21:25:38 +0530

    Add maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,3 +21,26 @@ allprojects {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("locus-android") {
+            groupId = "com.github.BirjuVachhani"
+            artifactId = "locus-android"
+            artifact("locus/build/outputs/aar/locus-release.aar")
+            pom {
+                withXml {
+                    val dependenciesNode = asNode().appendNode("dependencies")
+                    configurations.getByName("implementation") {
+                        dependencies.forEach {
+                            val dependencyNode = dependenciesNode.appendNode("dependency")
+                            dependencyNode.appendNode("groupId", it.group)
+                            dependencyNode.appendNode("artifactId", it.name)
+                            dependencyNode.appendNode("version", it.version)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
